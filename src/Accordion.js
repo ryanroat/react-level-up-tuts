0bd59d550a9c0e6513858f26934b972d7a7171a7
Ryan Roat <ryanroat@gmail.com>
2021-06-21 15:28:36 -0400

    comment tweaks for clarity

--- src/Accordion.js
+++ src/Accordion.js
@@ -3,10 +3,14 @@ import { useState } from 'react'
 export const Accordion = () => {
 
   const [ isToggled, setIsToggled ] = useState(false);
+
+  // const showMe = isToggled ? (<h3>Show Me</h3>) : null // option 1a
+
   return (
     <div>
-      {/* { isToggled && <h3>Show Me</h3> }  //option 1 */}
-      { isToggled ? <h3>Show Me</h3> : null }
+      {/* {showMe} // option 1b */}
+      {/* { isToggled ? <h3>Show Me</h3> : null } // option 2 */}
+      { isToggled && <h3>Show Me</h3> }  {/* option 3 */}
       <button
         onClick={ () => setIsToggled(!isToggled) }
       >Toggle</button>
