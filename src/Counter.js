1fe85484d9f8278748ae80b656205b9ade4dfabc
Ryan Roat <ryanroat@gmail.com>
2021-06-21 14:54:54 -0400

    implement add/subtrack Counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,19 @@
-
+import { useState } from 'react'
 
 export const Counter = () => {
+
+  // const [ stateName, setStateName ] = useState(defaultState/initialValue);
+  const [ count, setCount ] = useState(0);
+
   return (
     <div>
-      <h3>0</h3>
-      <button>+</button>
+      <h3>{count}</h3>
+      <button
+        onClick={() => setCount(count - 1)}
+      >-</button>
+      <button
+        onClick={() => setCount(count + 1)}
+      >+</button>
     </div>
   )
   }
-  
