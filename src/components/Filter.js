500efd5381a3598ffe5d8604542885949318f21e
Ryan Roat <ryanroat@gmail.com>
2021-06-29 14:41:43 -0400

    add proptypes to Filter and Movie

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import PropTypes from 'prop-types' 
 
 export const Filter = ({ filter, setFilter }) => {
   return (
@@ -13,4 +13,9 @@ export const Filter = ({ filter, setFilter }) => {
       </label>
     </div>
   )
+}
+
+Filter.propTypes = {
+  filter: PropTypes.string.isRequired,
+  setFilter: PropTypes.func.isRequired
 }
--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -1,7 +1,13 @@
-// import React from 'react'
+import PropTypes from 'prop-types' 
 
 export const Movie = ({ movie }) => {
   return (
     <li>{movie.name}</li>
   )
+}
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    name: PropTypes.string.isRequired
+  }).isRequired
 }
