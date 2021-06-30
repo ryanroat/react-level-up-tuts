395a992a6f2c481f1496fffdb6381966549ba4e5
Ryan Roat <ryanroat@gmail.com>
2021-06-30 11:10:29 -0400

    fetch API config and display poster images

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -1,13 +1,19 @@
 import PropTypes from 'prop-types' 
 
-export const Movie = ({ movie }) => {
+export const Movie = ({ movie, config }) => {
   return (
-    <li>{movie.title}</li>
+    <li>
+      {config.images?.base_url && (
+        <img src={config.images.base_url + config.images.poster_sizes[2] + movie.poster_path} alt={movie.title + " poster"} />
+      )}
+      <h3>{movie.title}</h3>
+    </li>
   )
 }
 
 Movie.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    poster_path: PropTypes.string.isRequired,
   }).isRequired
 }
--- src/movies/MoviesList.js
+++ src/movies/MoviesList.js
@@ -4,13 +4,16 @@ import { Filter } from '../components/Filter'
 import env from "react-dotenv"
 
 // const API_URL = your API_URL here or store in .env file for privacy
+// const CONFIG_URL = your CONFIG_URL here or store in .env file for privacy
 
 export const MoviesList = () => {
   const [filter, setFilter] = useState("")
   const [movies, setMovies] = useState([])
+  const [config, setConfig] = useState({})
 
   const getMovies = async () => {
     try {
+      // const res = await fetch(API_URL)
       const res = await fetch(env.API_URL)
       const movies = await res.json()
       setMovies(movies.results)
@@ -18,9 +21,20 @@ export const MoviesList = () => {
       console.error(error)
     }
   }
+  const getConfig = async () => {
+    try {
+      // const res = await fetch(CONFIG_URL)
+      const res= await fetch(env.CONFIG_URL)
+      const config = await res.json()
+      setConfig(config)
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   useEffect(() => {
     getMovies()
+    getConfig()
   }, [])
 
   return (
@@ -29,7 +43,7 @@ export const MoviesList = () => {
       <ul>
         { movies
           .filter(( movie ) => movie.title.toLowerCase().includes(filter.toLowerCase()))
-          .map(( movie ) => ( <Movie key={movie.id} movie={movie} /> ))
+          .map(( movie ) => ( <Movie key={movie.id} config={config} movie={movie} /> ))
         }
       </ul>
     </div>
